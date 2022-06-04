58398ecef1212a0a772cfa854b2eada31c5d5f14
Ethan Miller <ethanzanemiller@gmail.com>
2022-06-04 12:46:52 +0300

    Add the rest of the integration tests

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -88,6 +88,17 @@ const we_invoke_get_index = async () => {
   }
 };
 
+const we_invoke_get_restaurants = () => viaHandler({}, "get-restaurants");
+
+const we_invoke_search_restaurants = (theme) => {
+  let event = {
+    body: JSON.stringify({ theme }),
+  };
+  return viaHandler(event, "search-restaurants");
+};
+
 module.exports = {
   we_invoke_get_index,
+  we_invoke_get_restaurants,
+  we_invoke_search_restaurants,
 };
